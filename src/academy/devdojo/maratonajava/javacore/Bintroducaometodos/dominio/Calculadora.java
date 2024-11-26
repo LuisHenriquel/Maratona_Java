946dc9e347d8833815534fd110584cf557e53bc2
Luis <luishenriquehleme15@gmail.com>
2024-11-26 16:14:05 -0300

    Maratona_Java

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,9 +12,17 @@ public class Calculadora {
     }
 
     public void multiplicaDoisNumeros(int numero1, int numero2){
-        int total = numero1 + numero2;
-        System.out.println("A soma de " + numero1 + " + " + numero2 + " é: " + total);
+        int total = numero1 * numero2;
+        System.out.println("A multiplicação de " + numero1 + " x " + numero2 + " é: " + total);
     }
 
+
+    public double  divideDoisNumeros(double num1, double num2){
+        if(num2 == 0){
+            return 0;
+        }
+        double total = num1 / num2;
+        return  total;
+    }
 
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.divideDoisNumeros(6,2));
+
+    }
+
+
+
+
+
+
+}
